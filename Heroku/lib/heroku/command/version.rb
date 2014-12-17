17c4802b1b4586e47470373d01bfe5fa45064676
vkewalra <vkewalra@gmail.com>
2014-12-17 16:07:49 -0600

    Customize bootstrap variables

---
+++ Heroku/lib/heroku/command/version.rb
@@ -0,0 +1,28 @@
+require "heroku/command/base"
+require "heroku/version"
+
+# display version
+#
+class Heroku::Command::Version < Heroku::Command::Base
+
+  # version
+  #
+  # show heroku client version
+  #
+  #Example:
+  #
+  # $ heroku version
+  # heroku-toolbelt/1.2.3 (x86_64-darwin11.2.0) ruby/1.9.3
+  #
+  def index
+    validate_arguments!
+
+    display(Heroku.user_agent)
+
+    plugins = Heroku::Plugin.list
+    if plugins.length > 0
+      styled_header("Installed Plugins")
+      styled_array(plugins)
+    end
+  end
+end
